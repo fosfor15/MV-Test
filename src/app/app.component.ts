444c60379ad22a7ff980d08193e459460ad3eee8
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2023-11-24 14:28:21 +0300

    Update functionality and fix concurrent requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
-import { finalize, interval, map, mergeMap, take, tap } from 'rxjs';
+import { finalize, interval, mergeMap, of, take, tap } from 'rxjs';
 
 import { ApiMockServiceService } from './api-mock-service.service';
 
@@ -36,35 +36,40 @@ export class AppComponent {
      * @see apiMockService.postName$
      */
     handleSubmit(): void {
+        this.isNameShown = false;
+        this.isDisabled = true;
+
         this.apiMockService.postName$()
             .pipe(
-                map((responseStatus: boolean) => {
+                mergeMap((responseStatus: boolean) => {
                     if (responseStatus) {
                         this.name = this.form.controls['name'].value!;
                         this.isNameShown = true;
+                        this.form.reset();
+
+                        return of(true);
                     } else {
-                        this.isErrorShown = true;
+                        this.isErrorShown = this.isTimerShown = true;
+                        this.form.reset();
 
                         setTimeout(() => {
                             this.isErrorShown = false;
                         }, 5e3);
+
+                        return interval(1e3).pipe(
+                            take(this.timer),
+                            tap(() => {
+                                this.timer -= 1;
+                            }),
+                            finalize(() => {
+                                this.timer = 60;
+                                this.isTimerShown = false;
+                            })
+                        );
                     }
-
-                    this.form.reset();
                 }),
-                mergeMap(() => {
-                    this.timer = 60;
-                    this.isTimerShown = this.isDisabled = true;
-
-                    return interval(1e3).pipe(
-                        take(this.timer),
-                        tap(() => {
-                            this.timer -= 1;
-                        }),
-                        finalize(() => {
-                            this.isNameShown = this.isTimerShown = this.isDisabled = false;
-                        })
-                    );
+                finalize(() => {
+                    this.isDisabled = false;
                 })
             )
             .subscribe();
